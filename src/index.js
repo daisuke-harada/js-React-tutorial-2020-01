91596854dd0147e142763beeed465dcad53730ad
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-01-09 17:10:50 +0900

    [add] let,constによる変数宣言、オブジェクトの作成、配列の作成

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
-import "./styles.css";
+/**
+ * const, let等の変数宣言
+ */
+// var val1 = "var変数";
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// // var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// // var変数は再宣言可能
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// // letは上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// // letは再宣言不可能
+// // let val2 = "let変数を再宣言"
+
+// // const変数は上書き不可　再宣言も不可能
+// const val3 = "const変数";
+// console.log(val3);
+
+// constはオブジェクトの中身のプロパティは変更することができる。
+// const val4 = {
+//   name: "じゃけぇ",
+//   age: 28
+// };
+// val4.name = "daisuke";
+// val4.addres = "hirosima";
+// console.log(val4);
+
+//Reactでは主にconstを変数として使用する。
+const val5 = ["dog", "cat"];
+// 0番目の要素を入れ替える
+val5[0] = "bird";
+//配列を追加
+val5.push("monkey");
+console.log(val5);
